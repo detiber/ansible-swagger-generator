cb2835fb8ad0959e33be6557bb9497d8e18c4502
Jason DeTiberus <jdetiber@redhat.com>
2016-01-20 17:37:04 -0500

    Read and print swagger specs
    - Read in swagger specs from file or uri and dump info about the api

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup
+
+setup(
+    name = "ansible-swagger-generator",
+    version = "0.0.1",
+    author = "Jason DeTiberus",
+    author_email = "jdetiber@redhat.com",
+    description = ("Generate Ansible Modules form a Swagger spec"),
+    license = "Apache License, Version 2.0",
+    keywords = "Ansible Swagger",
+    url = "https://github.com/ansible-swagger-generator/ansible-swagger-generator",
+    packages=['ansible-swagger-generator'],
+    install_requires=['click', 'requests'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "Environment :: Console",
+        'Programming Language :: Python :: 2.7',
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
